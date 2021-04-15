3881da399c9bc3756c2e389291e066cb2646f733
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2021-04-15 16:43:52 +0700

    Amend (un)stage commands

--- edge/stageAll.ts
+++ edge/stageAll.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode'
 
 export default async function stageAll() {
+	await vscode.commands.executeCommand('workbench.view.scm')
 	await vscode.commands.executeCommand('git.stageAll')
 }
--- edge/unstageAll.ts
+++ edge/unstageAll.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode'
 
 export default async function unstageAll() {
+	await vscode.commands.executeCommand('workbench.view.scm')
 	await vscode.commands.executeCommand('git.unstageAll')
 }
