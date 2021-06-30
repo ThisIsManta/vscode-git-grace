19efa43eaa5950e1d2f89ea5b2c49845631721f9
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2021-06-30 16:43:29 +0700

    Use tab indentation

--- edge/extension.ts
+++ edge/extension.ts
@@ -27,82 +27,82 @@ import stageAll from './stageAll'
 import unstageAll from './unstageAll'
 
 export async function activate(context: vscode.ExtensionContext) {
-    // Prevent "No Git repository" error throwing from built-in Git extension
-    while (Git.getGitBuiltInExtension().isActive === false) {
-        await sleep(500)
-    }
+	// Prevent "No Git repository" error throwing from built-in Git extension
+	while (Git.getGitBuiltInExtension().isActive === false) {
+		await sleep(500)
+	}
 
-    context.subscriptions.push(vscode.workspace.onDidChangeWorkspaceFolders(updateStashCountBar))
+	context.subscriptions.push(vscode.workspace.onDidChangeWorkspaceFolders(updateStashCountBar))
 
-    context.subscriptions.push(vscode.workspace.onDidChangeWorkspaceFolders(Util.updateWorkspaceList))
+	context.subscriptions.push(vscode.workspace.onDidChangeWorkspaceFolders(Util.updateWorkspaceList))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.stageAll', Queue.put(stageAll)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.stageAll', Queue.put(stageAll)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.unstageAll', Queue.put(unstageAll)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.unstageAll', Queue.put(unstageAll)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.cleanAll', Queue.put(cleanAll)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.cleanAll', Queue.put(cleanAll)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.fetch', Queue.put(fetch)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.fetch', Queue.put(fetch)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.pull', Queue.put(pull)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.pull', Queue.put(pull)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.push', Queue.put(push)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.push', Queue.put(push)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.commitSmart', Queue.put(commitSmart, [commitSmart])))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.commitSmart', Queue.put(commitSmart, [commitSmart])))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.commitAmend', Queue.put(commitAmend)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.commitAmend', Queue.put(commitAmend)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.commitEmpty', Queue.put(commitEmpty)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.commitEmpty', Queue.put(commitEmpty)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.stash', Queue.put(stash)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.stash', Queue.put(stash)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.stashPopLatest', Queue.put(stashPopLatest)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.stashPopLatest', Queue.put(stashPopLatest)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.stashPop', Queue.put(stashPop)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.stashPop', Queue.put(stashPop)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.stashClear', Queue.put(stashClear)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.stashClear', Queue.put(stashClear)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.squash', squash))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.squash', squash))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.master', Queue.put(master, [fetch])))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.master', Queue.put(master, [fetch])))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.branch', Queue.put(branch)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.branch', Queue.put(branch)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.checkout', Queue.put(checkout, [fetch])))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.checkout', Queue.put(checkout, [fetch])))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.openWeb', Queue.put(openWeb)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.openWeb', Queue.put(openWeb)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.pullRequest', Queue.put(pullRequest)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.pullRequest', Queue.put(pullRequest)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.blame', Queue.put(blame)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.blame', Queue.put(blame)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.sync', Queue.put(sync)))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.sync', Queue.put(sync)))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.deleteBranch', Queue.put(async () => vscode.commands.executeCommand('git.deleteBranch'))))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.deleteBranch', Queue.put(async () => vscode.commands.executeCommand('git.deleteBranch'))))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.deleteMergedBranches', Queue.put(deleteMergedBranches, [fetch])))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.deleteMergedBranches', Queue.put(deleteMergedBranches, [fetch])))
 
-    context.subscriptions.push(vscode.commands.registerCommand('gitGrace.showOutput', () => {
-        Log.show()
-    }))
+	context.subscriptions.push(vscode.commands.registerCommand('gitGrace.showOutput', () => {
+		Log.show()
+	}))
 
-    const tortoiseGit = new TortoiseGit()
-    context.subscriptions.push(vscode.commands.registerCommand('tortoiseGit.showLog', Queue.put(() => tortoiseGit.showLog())))
-    context.subscriptions.push(vscode.commands.registerCommand('tortoiseGit.showFileLog', Queue.put(() => tortoiseGit.showFileLog())))
-    context.subscriptions.push(vscode.commands.registerCommand('tortoiseGit.commit', Queue.put(() => tortoiseGit.commit())))
-    context.subscriptions.push(vscode.commands.registerCommand('tortoiseGit.blame', () => tortoiseGit.blame()))
+	const tortoiseGit = new TortoiseGit()
+	context.subscriptions.push(vscode.commands.registerCommand('tortoiseGit.showLog', Queue.put(() => tortoiseGit.showLog())))
+	context.subscriptions.push(vscode.commands.registerCommand('tortoiseGit.showFileLog', Queue.put(() => tortoiseGit.showFileLog())))
+	context.subscriptions.push(vscode.commands.registerCommand('tortoiseGit.commit', Queue.put(() => tortoiseGit.commit())))
+	context.subscriptions.push(vscode.commands.registerCommand('tortoiseGit.blame', () => tortoiseGit.blame()))
 
-    if (vscode.workspace.workspaceFolders) {
-        await Queue.run(fetch)
-        await updateStashCountBar()
-    }
+	if (vscode.workspace.workspaceFolders) {
+		await Queue.run(fetch)
+		await updateStashCountBar()
+	}
 }
 
 export async function deactivate() {
-    Queue.clear()
+	Queue.clear()
 
-    if (Log) {
-        Log.hide()
-        Log.dispose()
-    }
+	if (Log) {
+		Log.hide()
+		Log.dispose()
+	}
 }
